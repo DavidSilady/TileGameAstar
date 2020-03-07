a3075730b3d9b114a16054b5bee147963790c88c
DavidSilady <david.silady@gmail.com>
2020-03-07 20:43:41 +0100

    Added match highlighting while moving the tiles

--- graphic_board.py
+++ graphic_board.py
@@ -6,19 +6,20 @@ from tkinter import *
 
 
 class GraphicBoard:
-	def __init__(self, model_board: Board, canvas):
-		self.model_board = model_board
+	def __init__(self, primary_board: Board, secondary_board: Board, canvas):
+		self.primary_board = primary_board
+		self.secondary_board = secondary_board
 		self.canvas = canvas
 
 	def draw_tiles(self, size, margin=5, goal_board: Board = None):
-		for y in range(self.model_board.height):
-			for x in range(self.model_board.width):
-				new_tile = self.Tile(x, y, self.model_board.matrix[x][y])
+		for y in range(self.primary_board.height):
+			for x in range(self.primary_board.width):
+				new_tile = self.Tile(x, y, self.primary_board.matrix[x][y])
 				new_tile.draw(self.canvas,
 				              (x * size) + (x * margin),
 				              (y * size) + (y * margin),
-				              self.model_board,
-				              goal_board=goal_board,
+				              self.primary_board,
+				              secondary_board=goal_board,
 				              size=size,
 				              margin=margin)
 
@@ -32,13 +33,17 @@ class GraphicBoard:
 			self.text = None
 			self.margin = 5
 
-		def mouse_clicked(self, event, model_board: Board, canvas):
-			direction_x, direction_y = model_board.interact(self.x, self.y)
+		def mouse_clicked(self, event, primary_board: Board, secondary_board: Board, canvas):
+			direction_x, direction_y = primary_board.interact(self.x, self.y)
 			self.move_tile(direction_x, direction_y, canvas, speed=100)
 			self.x += direction_x
 			self.y += direction_y
+			if primary_board.matrix[self.x][self.y] == secondary_board.matrix[self.x][self.y]:
+				canvas.itemconfig(self.background, fill="red")
+			else:
+				canvas.itemconfig(self.background, fill="black")
 
-		def draw(self, canvas: Canvas, x, y, initial_board: Board, goal_board: Board = None, size=50, margin=5):
+		def draw(self, canvas: Canvas, x, y, primary_board: Board, secondary_board: Board = None, size=50, margin=5):
 			self.margin = margin
 			self.size = size
 			if self.value == 0:
@@ -46,18 +51,24 @@ class GraphicBoard:
 			rect_bg = "black"
 			real_x = int(x/size)
 			real_y = int(y/size)
-			if goal_board is not None:
-				if initial_board.matrix[real_x][real_y] == goal_board.matrix[real_x][real_y]:
+			if secondary_board is not None:
+				if primary_board.matrix[real_x][real_y] == secondary_board.matrix[real_x][real_y]:
 					rect_bg = "red"
 			self.background = canvas.create_rectangle(x, y, x + size, y + size, fill=rect_bg, width=0)
 			center_x = x + (size / 2)
 			center_y = y + (size / 2)
 			self.text = canvas.create_text(center_x, center_y, text=str(self.value), fill="white", font="100")
 			canvas.tag_bind(self.background, "<Button-1>",
-			                lambda event: self.mouse_clicked(event, initial_board, canvas))
-			canvas.tag_bind(self.text, "<Button-1>", lambda event: self.mouse_clicked(event, initial_board, canvas))
+			                lambda event: self.mouse_clicked(event,
+			                                                 primary_board,
+			                                                 secondary_board,
+			                                                 canvas))
+			canvas.tag_bind(self.text, "<Button-1>", lambda event: self.mouse_clicked(event,
+			                                                                          primary_board,
+			                                                                          secondary_board,
+			                                                                          canvas))
 
-		def move_tile(self, direction_x, direction_y, canvas, speed=10):
+		def move_tile(self, direction_x, direction_y, canvas, speed=10, rect_bg="black"):
 			speed = 0.05 / speed
 			if self.value == 0:
 				return
--- view.py
+++ view.py
@@ -3,7 +3,7 @@ from tkinter import *
 from model import Board
 
 
-def init_gui(starting_board=None, size=75, width=3, height=3, margin=5):
+def init_gui(primary_board=None, size=75, width=3, height=3, margin=5):
 	canvas_width = width * (margin + size)
 	root_width = (canvas_width * 2) + size
 	root_height = height * (margin + size)
@@ -11,20 +11,20 @@ def init_gui(starting_board=None, size=75, width=3, height=3, margin=5):
 	root = Tk()
 	root.geometry(root_dimensions)
 
-	starting_board = Board(width, height)
-	goal_board = Board(width, height)
+	primary_board = Board(width, height)
+	secondary_board = Board(width, height)
 
-	work_canvas = Canvas(root, width=canvas_width, height=root_height)
-	work_canvas.pack(side='left')
+	primary_canvas = Canvas(root, width=canvas_width, height=root_height)
+	primary_canvas.pack(side='left')
 
-	goal_canvas = Canvas(root, width=canvas_width, height=root_height)
-	goal_canvas.pack(side='right')
+	secondary_canvas = Canvas(root, width=canvas_width, height=root_height)
+	secondary_canvas.pack(side='right')
 
-	graphic_work_board = GraphicBoard(starting_board, work_canvas)
-	graphic_work_board.draw_tiles(size=size, margin=margin, goal_board=goal_board)
+	primary_graphic_board = GraphicBoard(primary_board, secondary_board, primary_canvas)
+	primary_graphic_board.draw_tiles(size=size, margin=margin, goal_board=secondary_board)
 
-	graphic_goal_board = GraphicBoard(goal_board, goal_canvas)
-	graphic_goal_board.draw_tiles(size=size, margin=margin)
+	secondary_graphic_board = GraphicBoard(secondary_board, primary_board, secondary_canvas)
+	secondary_graphic_board.draw_tiles(size=size, margin=margin, goal_board=primary_board)
 
 	root.mainloop()
 
