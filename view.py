aef51e39cb21c55e359eaef4cb5f55a35a368c25
DavidSilady <david.silady@gmail.com>
2020-03-07 20:09:10 +0100

    Added second "goal" puzzle

--- view.py
+++ view.py
@@ -2,16 +2,28 @@ from graphic_board import *
 from tkinter import *
 
 
-def init_gui(board=None):
-	board = Board(5, 6)
+def init_gui(starting_board=None, size=75, width=3, height=3, margin=5):
+	canvas_width = width * (margin + size)
+	root_width = (canvas_width * 2) + size
+	root_height = height * (margin + size)
+	root_dimensions = str(root_width) + "x" + str(root_height)
 	root = Tk()
-	root.geometry('680x680')
+	root.geometry(root_dimensions)
 
-	canvas = Canvas(root, width=680, height=680, bg="white")
-	canvas.pack()
+	starting_board = Board(width, height)
+	goal_board = Board(width, height)
 
-	graphic_board = GraphicBoard(board, canvas)
-	graphic_board.draw_tiles(75)
+	work_canvas = Canvas(root, width=canvas_width, height=root_height)
+	work_canvas.pack(side='left')
+
+	goal_canvas = Canvas(root, width=canvas_width, height=root_height)
+	goal_canvas.pack(side='right')
+
+	graphic_work_board = GraphicBoard(starting_board, work_canvas)
+	graphic_work_board.draw_tiles(size=size, margin=margin)
+
+	graphic_goal_board = GraphicBoard(goal_board, goal_canvas)
+	graphic_goal_board.draw_tiles(size=size, margin=margin)
 
 	root.mainloop()
 
